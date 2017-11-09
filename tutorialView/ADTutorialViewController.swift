99c636b5ccb56613fbb8186c0cb32414bccf82d9
Adam Dong <andongd@ion360.com>
2017-11-09 11:52:36 +0800

    使用storyboard适配了iPhone X,复习了storyboard创建scrollView

---
+++ tutorialView/ADTutorialViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ADTutorialViewController.swift
+//  tutorialView
+//
+//  Created by 董安东 on 09/11/2017.
+//  Copyright © 2017 Adam. All rights reserved.
+//
+
+import UIKit
+
+class ADTutorialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func closeButtonAction(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ tutorialView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tutorialView
+//
+//  Created by 董安东 on 08/11/2017.
+//  Copyright © 2017 Adam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
